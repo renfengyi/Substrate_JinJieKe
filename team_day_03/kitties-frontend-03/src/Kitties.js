012ad67371a4a3a9eb4a91389274fe27a1c84684
renfengyi <447336761@qq.com>
2021-09-06 17:40:52 +0800

    修复点击创建按钮报错的问题
    修复点击创建按钮报错的问题

--- team_day_03/kitties-frontend-03/src/Kitties.js
+++ team_day_03/kitties-frontend-03/src/Kitties.js
@@ -16,43 +16,39 @@ export default function Kitties (props) {
   const [count,setCount] = useState()
   const [dnaarr,setDnaarr] = useState([])
   const [ownerarr,setOwnerarr] = useState([])
-  const [pop, setPop] = useState(false)
 
 
 
-  const fetchKitties = async () => {
+  const fetchKitties = () => {
     // TODO: 在这里调用 `api.query.kittiesModule.*` 函数去取得猫咪的信息。
     // 你需要取得：
     //   - 共有多少只猫咪
     //   - 每只猫咪的主人是谁
     //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
+    async function fetchdata(){
+      let kittiesList = [];
+      let ownerList = [];  
 
-    let kittiesList = [];
-    let ownerList = [];  
-
-    api.query.kittiesModule.kittiesCount(d=>{
-      if(d.toHuman()>0){
-        console.log(d.toHuman());
-        setCount(d.toHuman());
-        for(let i=0;i<d.toHuman();i++){
-          api.queryMulti([
-            [api.query.kittiesModule.owner,i],
-            [api.query.kittiesModule.kitties,i]
-          ],([ov,dv])=>{
-            kittiesList.push(dv.unwrap().toU8a());
-            //console.log("kittiesArr--"+JSON.stringify(kittiesArr))
-            setDnaarr(kittiesList);
-            ownerList.push(ov.unwrap().toString());
-            //console.log("ownerArr--"+JSON.stringify(ownerArr))
-            setOwnerarr(ownerList);
-
-            
-
-          });
+      api.query.kittiesModule.kittiesCount(d=>{
+        if(d.toHuman()>0){
+          console.log(d.toHuman());
+          setCount(d.toHuman());
+          for(let i=0;i<d.toHuman();i++){
+            api.queryMulti([
+              [api.query.kittiesModule.owner,i],
+              [api.query.kittiesModule.kitties,i]
+            ],([ov,dv])=>{
+              kittiesList.push(dv.unwrap().toU8a());
+              setDnaarr(kittiesList);
+              ownerList.push(ov.unwrap().toString());
+              setOwnerarr(ownerList);
+            });
+          }
         }
-      }
-    });
-
+      });
+    }
+    fetchdata()
+    
 
   }
 
@@ -66,29 +62,29 @@ export default function Kitties (props) {
     //  }, { id: ..., dna: ..., owner: ... }]
     //  ```
     // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
-    let _kitties = [];
-
-    //console.log("POP--",JSON.stringify(dnaarr),JSON.stringify(ownerarr));
-    setTimeout(()=>{
-      console.log("owner---"+JSON.stringify(ownerarr));
-      for(let i=0;i<count;i++){
-        let arr = {
-          id:i,
-          dna:dnaarr[i],
-          owner:ownerarr[i]
-        };
-        _kitties.push(arr);
-        //console.log(JSON.stringify(_kitties))
-      }
+    async function popKitties(){
+      let _kitties = [];
       setTimeout(()=>{
-        setKitties(_kitties);
-      },300);
-    },300);
+        console.log("owner---"+JSON.stringify(ownerarr));
+        for(let i=0;i<count;i++){
+          let arr = {
+            id:i,
+            dna:dnaarr[i],
+            owner:ownerarr[i]
+          };
+          _kitties.push(arr);
+        }
+        setTimeout(()=>{
+          setKitties(_kitties);
+        },300);
+      },300)
+    }
+    popKitties()
 
   }
 
-  useEffect(fetchKitties, [api, keyring,status])
-  useLayoutEffect(populateKitties, [dnaarr,ownerarr])
+  useEffect(fetchKitties, [api,keyring,status])
+  useEffect(populateKitties, [ownerarr,dnaarr,kitties,status])
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
